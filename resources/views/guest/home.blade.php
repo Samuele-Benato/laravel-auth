6c9b73b4c570ec1d73b31d5d7230fd788ccb1442
Samuele-Benato <samuele.benato@gmail.com>
2023-10-24 14:20:42 +0200

    feat : add start structure

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 
 @section('content')
-  <section class="container mt-5">
-    <h1>{{ $title }}</h1>
-  </section>
+    <section class="container mt-5">
+        <h1>{{ $title }}</h1>
+    </section>
 @endsection
